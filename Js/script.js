913dc7ddcbf537ff560cd5709a48b736026899d8
kibriaHassan <kibriahassan3500@gmail.com>
2025-12-09 10:31:13 +0600

    updates time

--- Js/script.js
+++ Js/script.js
@@ -13,3 +13,13 @@ document.getElementById("toggleBtn").addEventListener("click", function() {
     }
 });
 
+
+
+// Current Time Function
+function updateTime() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    document.getElementById('time').textContent = timeString;
+}
+setInterval(updateTime, 1000);
+updateTime();
